0513a95e77bbadae6cbd409f3372440dd180c1f2
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-27 14:40:49 -0400

    Initial dist subtree commit

--- src/storage.js
+++
@@ -1,7 +0,0 @@
-function populateStorage() {
-    localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
-    localStorage.setItem('font', document.getElementById('font').value);
-    localStorage.setItem('image', document.getElementById('image').value);
-  
-    setStyles();
-  }
